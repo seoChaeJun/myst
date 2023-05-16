045f561fc465158346410c54537ce9cea2ee7e88
seoChaeJun <121544528+seoChaeJun@users.noreply.github.com>
2023-05-16 11:35:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+rand = np.random.normal(1,2,size = 20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15) 
+st.pyplot(fig)
+       
+
+
